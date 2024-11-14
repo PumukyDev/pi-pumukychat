511c8cd6110ba5790760bb214f81715b449a44da
PumukyDev <adrian.bertosgomez@gmail.com>
2024-11-14 12:25:11 +0100

    refactor: code changes to make it clear
    Refactor the code

--- htdocs/pages/index.php
+++ htdocs/pages/index.php
@@ -1,11 +1,11 @@
 <?php
 $pageTitle = "PumukyDev - Página Principal";
 include '../includes/header.php';
-
-echo "<main>";
-    echo "<h1>Bienvenido a PumukyDev</h1>";
-    echo "<p>Esta es la página principal de PumukyDev. La página está aún en desarrollo, ten paciencia por favor.</p>";
-echo "</main>";
-
-include '../includes/footer.php';
 ?>
+
+<main>
+    <h1>Bienvenido a PumukyDev</h1>
+    <p>Esta es la página principal de PumukyDev. La página está aún en desarrollo, ten paciencia por favor.</p>
+</main>
+
+<?php include '../includes/footer.php';?>
