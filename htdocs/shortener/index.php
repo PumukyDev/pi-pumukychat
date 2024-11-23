c3ae9a4d537b2b33bcda38549600434b6bf631eb
PumukyDev <adrian.bertosgomez@gmail.com>
2024-11-23 11:32:46 +0100

    feat: add hash converter
    Add a form to convert long url into a hash for later operations

--- htdocs/shortener/index.php
+++ htdocs/shortener/index.php
@@ -6,6 +6,13 @@ include '../includes/header.php';
 <main>
     <h1>Acortador de URL</h1>
     <p>Usa esta herramienta para acortar tus enlaces.</p>
+
+    <form action="urlshortener.php" method="POST">
+        <label for="long_url">URL larga:</label>
+        <input type="text" id="long_url" name="long_url" required/>
+        <br>
+        <button type="submit">Acortar</button>
+    </form>
 </main>
 
 <?php include '../includes/footer.php';?>
