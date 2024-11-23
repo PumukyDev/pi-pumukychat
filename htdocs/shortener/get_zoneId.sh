6ab1ca79b1bbaf40157e32ebb9dc744ec59c5674
PumukyDev <adrian.bertosgomez@gmail.com>
2024-11-23 12:01:43 +0100

    fiz: change a variable name
    Change zoneId name to follow the same pattern with all the variables

--- htdocs/shortener/get_zoneId.sh
+++ htdocs/shortener/get_zoneId.sh
@@ -14,8 +14,8 @@ zoneId=$(echo "$zoneInfo" | jq -r '.[0].id')
 
 # Verify if the extraction was successful
 if [[ -n "$zoneId" ]]; then
-    echo "ID_ZONE=$zoneId" >> .env
-    echo "ID_ZONE=$zoneId"
+    echo "zoneId=$zoneId" >> .env
+    echo "zoneId=$zoneId"
 else
     echo "Error: can't get the zone id."
     echo "Info: $zoneInfo"
