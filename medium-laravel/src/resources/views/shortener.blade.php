bcbfd34c4121e7c48d8005f8bdecd8c18f1639ee
PumukyDev <adrian.bertosgomez@gmail.com>
2025-01-03 16:26:28 +0000

    feat: add shortened urls table Add shortened urls table with the following command:
    php artisan make:migration create_shortened_urls_table
    
    Add original_url and shortened_url fields too.

--- medium-laravel/src/resources/views/shortener.blade.php
+++ medium-laravel/src/resources/views/shortener.blade.php
@@ -5,15 +5,12 @@
 @section('content')
     <h1>URL shortener</h1>
     <div id="url-shortener">
-        <form action="index.php" method="POST">
+        <form action="/shortener" method="POST">
+            @csrf
             <label for="long_url">URL to shorten:</label>
-            <input type="url" id="long_url" name="long_url" required/>
+            <input type="url" id="long_url" name="long_url" required>
             <button type="submit">Shorten</button>
         </form>
-
-        <?php if (isset($message)) { ?>
-            <div class="message"><?= $message; ?></div>
-        <?php } ?>
     </div>
 
 @endsection
