a8dc4a964a4b38be6f856a68907cc6015ff1abd9
PumukyDev <adrian.bertosgomez@gmail.com>
2025-01-03 17:48:45 +0100

    feat: extend url shortener model Add some data to be complete in the url shortener table

--- medium-laravel/src/app/Models/ShortenedUrl.php
+++ medium-laravel/src/app/Models/ShortenedUrl.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShortenedUrl extends Model
 {
-    //
+    // Table name
+    protected $table = 'url_shortener';
+
+    // Columns
+    protected $fillable = ['original_url', 'shortened_url'];
+
+    // Function to generate the short URL
+    public static function generateShortenedUrl($originalUrl)
+    {
+        // Create a hash
+        $shortenedUrl = substr(hash('sha256', $originalUrl), 0, 6);
+
+        // Save it in the database
+        return self::create([
+            'original_url' => $originalUrl,
+            'shortened_url' => $shortenedUrl
+        ]);
+    }
 }
