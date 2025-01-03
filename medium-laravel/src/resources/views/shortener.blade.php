59eb91159a79b9699a5377e4f4d8f7a982dee736
PumukyDev <adrian.bertosgomez@gmail.com>
2025-01-03 14:54:10 +0000

    feat: add shortener form Add previous url-shortener form to shorten links

--- medium-laravel/src/resources/views/shortener.blade.php
+++ medium-laravel/src/resources/views/shortener.blade.php
@@ -4,5 +4,16 @@
 
 @section('content')
     <h1>URL shortener</h1>
+    <div id="url-shortener">
+        <form action="index.php" method="POST">
+            <label for="long_url">URL to shorten:</label>
+            <input type="url" id="long_url" name="long_url" required/>
+            <button type="submit">Shorten</button>
+        </form>
+
+        <?php if (isset($message)) { ?>
+            <div class="message"><?= $message; ?></div>
+        <?php } ?>
+    </div>
 
 @endsection
