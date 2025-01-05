a3103372a8128b0a2af2e0aa395511697c68926e
PumukyDev <adrian.bertosgomez@gmail.com>
2025-01-05 11:53:20 +0100

    feat: enable chat authentication Create authentication page to create and log users.

--- server/src/resources/views/chat.blade.php
+++ server/src/resources/views/chat.blade.php
@@ -4,4 +4,20 @@
 
 @section('content')
     <h1>Encrypted Chat</h1>
+
+    <!-- Check login status of a user -->
+    @if (Route::has('login'))
+        <nav>
+            @auth
+                <p>The chat is not working at the moment, please be patient</p>
+            @else
+                <a href="{{ route('login') }}">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </nav>
+    @endif
+
 @endsection
--- server/src/routes/web.php
+++ server/src/routes/web.php
@@ -10,11 +10,11 @@ Route::get('/', function () {
 
 Route::get('/chat', function () {
     return view('chat');
-});
+})->name('chat');;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
