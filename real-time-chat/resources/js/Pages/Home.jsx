7a72061411dd1e6b6a28e2e84d41fad4b652d955
PumukyDev <adrian.bertosgomez@gmail.com>
2025-03-21 18:47:09 +0100

    feat: Create layout to render messages
    - Create a gap for messages to be rendered
    - Add default message if you have no messages yet
    - Try to create a mechanism to delete messages without deleting entire
    conversations

--- real-time-chat/resources/js/Pages/Home.jsx
+++ real-time-chat/resources/js/Pages/Home.jsx
@@ -1,10 +1,60 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import ChatLayout from '@/Layouts/ChatLayout';
-import { Head } from '@inertiajs/react';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
+import { useRef, useState, useEffect } from 'react';
 
-function Home() {
+function Home({ Messsages }) {
+
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
+
+    useEffect(() => {
+        setLocalMessages(messages);
+    }, [messages]);
+
     return (
-        <>Messages</>
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-state-200">
+                        Please select conversation to see messages
+                    </div>
+                    <ChatBubbleLeftRightIcon classNmae="w-32 h-32 inline-block" />
+                </div>
+            )}
+            {messages && (
+                <>
+                    <ConversationHeader
+                        selectedConversation={selectedConversation}
+                    />
+                    <div
+                        ref={messagesCtrRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {/* Messages */}
+
+                        {localMessages.lenght === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+                        {localMessages.lenght > 0 && (
+                            <div className="flex-1 flex-col">
+                                {localMessages.map((message) => (
+                                    <MessageItem
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation} />
+                </>
+            )}
+        </>
     );
 }
 
