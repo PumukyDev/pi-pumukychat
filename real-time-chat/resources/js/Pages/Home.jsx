2aa02bf5c5f3262f331121224847dda4e05f49d9
PumukyDev <adrian.bertosgomez@gmail.com>
2025-03-29 15:27:42 +0100

    feat: Add message icons to the bottom bar

--- real-time-chat/resources/js/Pages/Home.jsx
+++ real-time-chat/resources/js/Pages/Home.jsx
@@ -11,6 +11,13 @@ function Home({ selectedConversation = null, messages = null }) {
     const messagesCtrRef = useRef(null);
 
     useEffect(() => {
+        setTimeout(() => {
+        messagesCtrRef.current.scrollTop =
+            messagesCtrRef.current.scrollHeight;
+        }, 10);
+    }, [selectedConversation]);
+
+    useEffect(() => {
         setLocalMessages(messages ? messages.data.reverse() : []);
     }, [messages]);
 
@@ -53,7 +60,7 @@ function Home({ selectedConversation = null, messages = null }) {
                             </div>
                         )}
                     </div>
-                    {/* <MessageInput conversation={selectedConversation} /> */}
+                    <MessageInput conversation={selectedConversation} />
                 </>
             )}
         </>
