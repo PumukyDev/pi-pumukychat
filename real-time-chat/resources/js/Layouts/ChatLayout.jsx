fd459eb913bd38d64d32f17f41d1e8839a17909e
PumukyDev <adrian.bertosgomez@gmail.com>
2025-04-07 19:11:09 +0200

    feat: Update content in message menu after receiving a message
    After receiving a message the messages menu will be automatically
    updated with the own message and the date.

--- real-time-chat/resources/js/Layouts/ChatLayout.jsx
+++ real-time-chat/resources/js/Layouts/ChatLayout.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { PencilSquareIcon } from '@heroicons/react/24/solid'
 import TextInput from "@/Components/TextInput";
 import ConversationItem from "@/Components/App/ConversationItem";
+import { useEventBus } from "@/EventBus";
 
 
 const ChatLayout = ({ children}) => {
@@ -12,6 +13,7 @@ const ChatLayout = ({ children}) => {
     const [localConversations, setLocalConversations] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
     const [onlineUsers, setOnlineUsers] = useState({});
+    const { on } = useEventBus();
 
     const isUsersOnline = (userId) => onlineUsers[userId];
 
@@ -26,8 +28,40 @@ const ChatLayout = ({ children}) => {
         );
     }
 
-    console.log("conversation:", conversations)
-    console.log("selectedConversation", selectedConversation)
+    const messageCreated = (message) => {
+        setLocalConversations((oldUsers) => {
+            return oldUsers.map((u) => {
+                // If the message is for user
+                if (
+                    message.receiver_id &&
+                    !u.is_group &&
+                    (u.id == message.sender_id || u.id == message.receiver_id)
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                // If the message is for group
+                if (
+                    message.group_id &&
+                    u.is_group &&
+                    u.id == message.group_id
+                ) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                return u;
+            });
+        });
+    }
+
+    useEffect(() => {
+        const offCreted = on("message.created", messageCreated);
+        return () => {
+            offCreated();
+        };
+    }, [on]);
 
     useEffect(() => {
         setSortedConversations(
