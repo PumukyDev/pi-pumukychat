e7f742866819884e2f47f4e61e2a63bcd36d9bfd
PumukyDev <adrian.bertosgomez@gmail.com>
2025-04-12 00:44:18 +0200

    feat: Add functions to check file types and  size

--- real-time-chat/resources/js/helpers.jsx
+++ real-time-chat/resources/js/helpers.jsx
@@ -54,3 +54,46 @@ export const isYesterday = (date) => {
         date.getFullYear === yesterday.getFullYear()
     );
 };
+
+export const isImage = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "image";
+};
+
+export const isAudio = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    mime = mime.split("/");
+    return mime[0].toLowerCase() === "audio";s
+}
+
+export const isPDF = (attachment) => {
+    let mime = attachment.mime || attachment.type;
+    return mime === "application/pdf";
+}
+
+export const isPreviewable = (attachment) => {
+    return (
+        isImage(attachment) ||
+        isVideo(attachment) ||
+        isAudio(attachment) ||
+        isPDF(attachment)
+    );
+};
+
+export const formatBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return "0 bytes";
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB"];
+
+    let i = 0;
+    let size = bytes;
+    while (size >= k) {
+        size /= k;
+        i++;
+    }
+
+    return parseFloat(size.toFixed(dm)) + " " + sizes[i];
+}
