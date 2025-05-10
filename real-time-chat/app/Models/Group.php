e2ac063584640381413e4fbdcc10f1d905267394
PumukyDev <adrian.bertosgomez@gmail.com>
2025-05-10 11:25:57 +0200

    fix: Fix typo

--- real-time-chat/app/Models/Group.php
+++ real-time-chat/app/Models/Group.php
@@ -38,14 +38,14 @@ class Group extends Model
 
     public static function getGroupsForUser(User $user)
     {
-        $quary = self::select('groups.*', 'messages.message as last_message', 'messages.created_at as last_message_date')
+        $query = self::select('groups.*', 'messages.message as last_message', 'messages.created_at as last_message_date')
             ->join('group_users', 'group_users.group_id', '=', 'groups.id')
             ->leftJoin('messages', 'messages.id', '=', 'groups.last_message_id')
             ->where('group_users.user_id', $user->id)
             ->orderBy('messages.created_at', 'desc')
             ->orderBy('groups.name');
 
-        return $quary->get();
+        return $query->get();
     }
 
     public function toConversationArray()
